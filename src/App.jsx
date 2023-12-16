e42389f4dc5f6dc052f4bddf888d6d64ba6ac9d7
phildeoner <phildeoner@gmail.com>
2023-12-16 23:38:43 +0100

    Create basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import CallToAction from "./components/CallToAction";
+import Checkout from "./components/Checkout";
+import Footer from "./components/Footer";
+import ImageSlide from "./components/ImageSlide";
+import NavBar from "./components/NavBar";
+
 function App() {
-  return <></>;
+  return (
+    <div className="bg-[#F5F5F5] text-gray-900">
+      <NavBar />
+      <Checkout />
+      <ImageSlide />
+      <CallToAction />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ImageSlide.jsx
@@ -0,0 +1,35 @@
+function ImageSlide() {
+  return (
+    <>
+      <div className="relative w-full flex gap-4 py-6 overflow-x-auto">
+        <img
+          className="h-48 aspect-video rounded-sm object-cover object-center dark:bg-gray-500"
+          src="https://s3-alpha-sig.figma.com/img/2438/62e6/926e116640ee6b332250c62e0bed2894?Expires=1703462400&Signature=jU3Pi88JiFeomDOgbNtRYfKhkzC3tDUiW-trwsiIcjE7jjY~u0wzQBIzhFDPwNwBwIFwbhq6JxifhUxuGbMwL4QBMNZ8AwKBgIYaliNfaoWdK4~2nz2jNpXMECMAtkm0LY9rBpfKQSgws7GvEXYF7cD8ZOz1ndpZeceuuG520fn9xbVfpd0DjN8YPPvIx5VdXLMrdsRAqR-w5h0bykjvP6357rQJhWxtXMRcXUYlcRA8Q0eDM0Z0VxfgLQhOQada668oQhZWcz~61~ZsKTh2C2BCXiEEqHwW4o5BSf7ZSJb-uZeTWAYy9v4eUFMoXO5ZDwnThzAUkpbKfkTNnKBO-w__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+          alt="Image 1"
+        />
+        <img
+          className="h-48 aspect-video rounded-sm object-cover object-center dark:bg-gray-500"
+          src="https://s3-alpha-sig.figma.com/img/9731/266b/0e6b1539332b0d0b872c05ca560e6853?Expires=1703462400&Signature=hB6DgHQP4AfSnWITddlDvrjdNmjIOB-nlqA1Iv6X9PoahGDNR4V1SsPwbA8Ylncrqp7NsTKZePXI2tw1aOIui~Jfnq40TehcZFzYWn3tB5e8KwSoFH~IoCITJwwq0HeXBfLoyj0HKyamR9bHmmdStIjumR2cuvH-gGRM9jvdaSo0gmmV~WQHKjGioIQ1hmu1hzvXzb-6Esaeb3Tvk~2xZfIdjVD-bSdF2I7VxBxp4rL2GaRSNPPbKIMttGNvRj2mfLmznSE7OlPHQQb4FpCGIJWjXeAc-2hoq0JqqoYVPnI1LT6ejcafRNy~zqf5JgK4QKz8Jcpxc~p-PNKpEf91xw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+          alt="Image 2"
+        />
+        <img
+          className="h-48 aspect-video rounded-sm object-cover object-center dark:bg-gray-500"
+          src="https://s3-alpha-sig.figma.com/img/1098/0e73/b8a0c489adf285dd0c7ea3441805c70d?Expires=1703462400&Signature=ahwhjZWdvTMPTpaFc5-GE~JXqwjZktv7OPDcwqkmOhWFZ86lm9JqMvtdDk8A1gVxrkOO1ClixNWcjQS7M5LzeGIPTe0Y~fl6vSYxzS-FxCjD5s6A3iwXk22LNHLnZ9Fsz4oA5D7cxxI-J-FzOkzMoUBp1uKvOt9Wd6np3p0wfFcusc3FEaRnm98VpqFtrz17IoJVVYYt-bLmtjQpXUIDqYkG7JUUKzmAu-aAtoe5q8BsevT~pgMYN0mLix6KGH9Zu9IVYSWQl~mhXRfwkdbnNssK5UG1Kh6K29RSsekIYpvrIRLxRRzEB8nnW5bp4uOZLstQ9dGwAmjR4gXiNu6WYg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+          alt="Image 3"
+        />
+        <img
+          className="h-48 aspect-video rounded-sm object-cover object-center dark:bg-gray-500"
+          src="https://s3-alpha-sig.figma.com/img/7071/2aad/8154c4f72cddf1fcf7f431091f08a30c?Expires=1703462400&Signature=VJdqBMy6bcjTSw8qZj6snRn~seMz1SQXNtrCRHSNtqZDowVsqxHQOzFKg5jwN1NQ5wo~EBYXpwjYKpJcHyqNNukWwGjcirYj7YeiBgJB~EnjITYkQAKShpaj7q-lmCr37UhO5o-Eop-Kdk91-9s4b~hNNG-rvn6mv4-d~wQhkY6MUy4zwI1qzqpJx3L5qXYzl7Qad3W5VsTDgW4Wa3Hbyc80k~euLaXT9pFXELcRJ2td7~M7qKNXEj2Z2UFwy24L3dCZO0rsUBt64qYoxLUKuByYbj7MwBz9Vwl74m4HavOWq~J9Zvwvn~DeS-WLiiwacWBMbwXir7ieuUOXFQ2pDA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+          alt="Image 4"
+        />
+        <img
+          className="h-48 aspect-video rounded-sm object-cover object-center dark:bg-gray-500"
+          src="https://s3-alpha-sig.figma.com/img/bc3c/a273/83db0a289f0504457e532bb4fc23a2a6?Expires=1703462400&Signature=DcjyDohf8LD831WVyxICbmYc~5oBrP9qDTcHPKEJGNN-osi7s7JW57kVM5QHNJOiTSVOSnNJoC-16JByIwSRzptxqmSoLW0pg3lynyyGyo9ErYkczPUSG03IDJHHKeNrn919WVkk6PWE1kGmGcGIbeutij5uVvMTl2XA1ORLl~4QGq0nZ5zmAXaCnMbQfZFWb8hiJJp93Myn42vr32L6T6CcO6HWgE8I~enwDqxnwgdDzFTMyZE3cvB2D1SVldzt3BOpYrwy04eLN5RwDBiJljj87peHeVbyqsR7n8tgkG~EwbCI9Apg~SGPz6mJG9bwvBFsbNEwfDjtA2a86zN2Gw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+          alt="Image 5"
+        />
+      </div>
+    </>
+  );
+}
+
+export default ImageSlide;
